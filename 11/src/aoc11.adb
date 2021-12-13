4ce8d85a70e2a01598be2b99b4b1748b29dce5bd
Basler Corentin EXT <Corentin.Basler@CIMO.CH>
2021-12-13 16:57:11 +0100

    Finishing day 11

--- 11/src/aoc11.adb
+++ 11/src/aoc11.adb
@@ -1,8 +1,6 @@
 with Ada.Text_IO.Editing;   use Ada.Text_IO;
 with Utils;
 
-use type Utils.unsigned_64;
-
 procedure aoc11 is
 
   type Octopus is
@@ -16,10 +14,13 @@ procedure aoc11 is
   type Octopus_Matrix is array(Integer range <>) of Octopus_Array(1..10);
 
   file     : File_Type;
-  fileName : constant string := "/home/Pacco/AdventOfCode2021/11/src/inputTest.txt";
+  fileName : constant string := "D:\Install\aoc\11\src\input.txt";
   count    : Integer := 1;
   octopuses : Octopus_Matrix(1..10) := (others => (others => Null_Octopus));
-  flashCount : Utils.unsigned_64 := 0;
+  flashCount : Integer := 0;
+  allFlashed : Boolean := False;
+  firstAllFlash : Integer := 0;
+  stepCount  : Integer := 1;
 
   procedure printOctopuses (octopuses : in out Octopus_Matrix) is
     package Int_IO is new Integer_IO(Integer);
@@ -46,65 +47,78 @@ procedure aoc11 is
     Ada.Text_IO.New_Line;
   end printOctopusesFlashes;
 
-  function isInRange(x : in Integer;
-                     y : in Integer) return Boolean is
+  function isInRange(row : in Integer;
+                     col : in Integer) return Boolean is
   begin
-    return (y in octopuses'Range and then x in octopuses(y)'Range);
+    return (row in octopuses'Range and then col in octopuses(row)'Range);
   end isInRange;
 
   procedure flash(octopuses  : in out Octopus_Matrix;
-                  flashCount : in out Utils.unsigned_64;
-                  x          : in Integer;
-                  y          : in Integer) is
+                  --flashCount : in out Utils.unsigned_64;
+                  flashCount : in out Integer;
+                  row        : in Integer;
+                  col        : in Integer) is
   begin
-    octopuses(y)(x).value := octopuses(y)(x).value + 1;
-    if (octopuses(y)(x).flashed) then
+    octopuses(row)(col).value := octopuses(row)(col).value + 1;
+
+    if (octopuses(row)(col).flashed) then
       return;
-    elsif(octopuses(y)(x).value > 9) then
+    elsif(octopuses(row)(col).value > 9) then
       flashCount := flashCount + 1;
-      octopuses(y)(x).flashed := true;
+      octopuses(row)(col).flashed := true;
       -- go thougt neighbours
       -- top right
-      if (isInRange (x-1, y-1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x-1, y => y-1);
+      if (isInRange (row-1, col-1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row-1, col => col-1);
       end if;
       -- top middle
-      if (isInRange (x, y-1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x, y => y-1);
+      if (isInRange (row-1, col)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row-1, col => col);
       end if;
       --top left
-      if (isInRange (x, y-1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x, y => y-1);
+      if (isInRange (row-1, col+1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row-1, col => col+1);
       end if;
       -- middle left
-      if (isInRange (x-1, y)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x-1, y => y);
+      if (isInRange (row, col-1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row, col => col-1);
       end if;
       -- middle right
-      if (isInRange (x+1, y)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x+1, y => y);
+      if (isInRange (row, col+1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row, col => col+1);
       end if;
       -- bottom left
-      if (isInRange (x-1, y+1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x-1, y => y+1);
+      if (isInRange (row+1, col-1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row+1, col => col-1);
       end if;
       -- bottom middle
-      if (isInRange (x, y+1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x, y => y+1);
+      if (isInRange (row+1, col)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row+1, col => col);
       end if;
       -- bottom left
-      if (isInRange (x+1, y+1)) then
-        flash (octopuses => octopuses, flashCount => flashCount, x => x+1, y => y+1);
+      if (isInRange (row+1, col+1)) then
+        flash (octopuses => octopuses, flashCount => flashCount, row => row+1, col => col+1);
       end if;
-      return;
     else
-      --octopuses(x)(y).value := octopuses(x)(y).value + 1;
       return;
     end if;
-  end;
+  end flash;
+
+  function hasAllFlashed( octopuses : in Octopus_Matrix) return Boolean is
+  begin
+    for I in octopuses'Range loop
+      for J in octopuses(I)'Range loop
+        if (not octopuses(I)(J).flashed) then
+          return false;
+        end if;
+      end loop;
+    end loop;
+    return true;
+  end hasAllFlashed;
 
   procedure runStep (octopuses  : in out Octopus_Matrix;
-                     flashCount : in out Utils.unsigned_64) is
+                     allFlashed : in out Boolean;
+                     flashCount : in out Integer) is
   begin
     -- Increment all octopuses
     for I in octopuses'Range loop
@@ -112,16 +126,17 @@ procedure aoc11 is
         octopuses(I)(J).value := octopuses(I)(J).value + 1;
       end loop;
     end loop;
-    printOctopuses (octopuses);
+
     -- Compute Flashes
     for I in octopuses'Range loop
       for J in octopuses(I)'Range loop
         if(octopuses(I)(J).value > 9 and not octopuses(I)(J).flashed) then
-          flash (octopuses => octopuses, flashCount => flashCount, x => J, y => I);
+          flash (octopuses => octopuses, flashCount => flashCount, row => I, col => J);
         end if;
       end loop;
     end loop;
-    printOctopusesFlashes(octopuses);
+
+    allFlashed := hasAllFlashed(octopuses);
     -- Set 0 and reset Flashes
     for I in octopuses'Range loop
       for J in octopuses(I)'Range loop
@@ -141,7 +156,6 @@ begin
     declare
       line : String := Get_Line(file);
     begin
-      Ada.Text_IO.Put_Line (line);
       for I in line'Range loop
         octopuses(count)(I).value := Character'Pos(line((I))) - 48;
       end loop;
@@ -149,13 +163,24 @@ begin
     end;
   end loop;
   -- Run steps
-  for S in 1 .. 2 loop
-    Ada.Text_IO.Put_Line(Integer'Image(S));
-    runStep (octopuses, flashCount);
-    if(S = 1 or S = 2) then
-      printOctopuses (octopuses);
-    end if; 
+  for S in 1 .. 100 loop
+    --Ada.Text_IO.Put_Line (Integer'Image(S));
+    runStep (octopuses, allFlashed, flashCount);
+    stepCount := S;
   end loop;
-  Ada.Text_IO.Put_Line (Utils.unsigned_64'Image(flashCount));
+
+  loop
+    runStep (octopuses, allFlashed, flashCount);
+    stepCount := stepCount + 1;
+    if (firstAllFlash = 0 and allFlashed) then
+      firstAllFlash := stepCount;
+      exit;
+    end if;
+  end loop;
+
+  --Ada.Text_IO.Put_Line (Utils.unsigned_64'Image(flashCount));
+  Ada.Text_IO.Put_Line (Integer'Image(flashCount));
+  Ada.Text_IO.Put_Line ("aoc11 part2");
+  Ada.Text_IO.Put_Line (Integer'Image(firstAllFlash));
 
 end aoc11;
